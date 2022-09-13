bea48bf6623de940bad2d6da3caec2728b045917
Ross Eyre <rosseyre@gmail.com>
2022-09-13 18:49:13 +0300

    added toggle image

---
+++ js/toggle.js
@@ -0,0 +1,15 @@
+const img_path_beef = 'assets/imgs/beef-production.png'
+const img_path_soy = 'assets/imgs/soy-production.png'
+
+var img_path = img_path_beef // default
+
+const toggle_switch = document.querySelector('.toggle-switch')
+const toggle_image = document.querySelector('#toggle-image')
+
+toggle_switch.addEventListener('change', (e) => {
+  if (e.target.checked) {
+    toggle_image.src = img_path_soy
+  } else {
+    toggle_image.src = img_path_beef
+  }
+})
